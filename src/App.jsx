299a870165ea11683bf2bc2f26d216c68d87c623
Olha Sheliakina <olya.shelyakina@gmail.com>
2023-10-26 22:35:47 +0300

    adding lazy loading and loader component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import 'bulma/css/bulma.css';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, lazy, Suspense } from 'react';
 import Navigation from './components/Navigation';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './tabs/Home';
+import Loader from './components/Loader';
+
+function renderLazyComponent(path) {
+  const LazyComponent = lazy(() => import(`${path}`));
+  return <LazyComponent />;
+}
 
 const App = () => {
   const [tabs, setTabs] = useState([]);
@@ -26,25 +32,29 @@ const App = () => {
 
       <main>
         <div>
-          <Routes>
-            <Route path='/home' element={<Home />} />
-            <Route index element={<Navigate to='/home' replace />} />
-            {tabs.map((tab) => (
+          <Suspense
+            fallback={<Loader />}
+          >
+            <Routes>
+              <Route path='/home' element={<Home />} />
+              <Route index element={<Navigate to='/home' replace />} />
+              {tabs.map((tab) => (
+                <Route
+                  key={tab.id}
+                  path={`${tab.id}`}
+                  element={renderLazyComponent(tab.path)}
+                />
+              ))}
               <Route
-                key={tab.id}
-                path={`${tab.id}`}
-                element={tab.title}
+                path='*'
+                element={
+                  <h1 className='title has-text-centered m-6'>
+                    Page was not found
+                  </h1>
+                }
               />
-            ))}
-            <Route
-              path='*'
-              element={
-                <h1 className='title has-text-centered m-6'>
-                  Page was not found
-                </h1>
-              }
-            />
-          </Routes>
+            </Routes>
+          </Suspense>
         </div>
       </main>
     </div>
