b92d8a5d5f00e308e4220ef54d54b0e6525e4c5e
Olha Sheliakina <olya.shelyakina@gmail.com>
2023-10-26 20:59:15 +0300

    adding ProjectsInfo component

--- src/tabs/ProjectsInfo.jsx
+++ src/tabs/ProjectsInfo.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const ProjectsInfo = () => {
-  return (
-    <div>ProjectsInfo</div>
-  )
-}
+  const [projectsData, setProjectsData] = useState([]);
 
-export default ProjectsInfo
+  useEffect(() => {
+    fetch('http://localhost:3000/data/projectsData.json')
+      .then((response) => response.json())
+      .then((data) => {
+        setProjectsData(data.projectsInfo);
+      })
+      .catch((error) => {
+        console.error('Error has occured while fetching data:', error);
+      });
+  }, []);
+
+  const getStatusClass = (status) => {
+    switch (status) {
+      case 'In Progress':
+        return 'has-background-warning'; 
+      case 'Completed':
+        return 'has-text-success';
+      case 'On Hold':
+        return 'has-text-danger'; 
+      default:
+        return '';
+    }
+  };
+
+  return (
+    <section className='section is-medium has-text-centered'>
+      <div className='table-container'>
+        <table className='table is-fullwidth'>
+          <thead>
+            <tr>
+              <th className='has-text-centered'>Project Name</th>
+              <th className='has-text-centered'>Project Status</th>
+              <th className='has-text-centered'>Project Start Date</th>
+              <th className='has-text-centered'>Project End Date</th>
+            </tr>
+          </thead>
+          <tbody>
+          {projectsData.map((project) => (
+            <tr key={project.id} className={getStatusClass(project.status)}>
+              <td>{project.name}</td>
+              <td>{project.status}</td>
+              <td>{project.startDate}</td>
+              <td>{project.endDate}</td>
+            </tr>
+          ))}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+};
+
+export default ProjectsInfo;
