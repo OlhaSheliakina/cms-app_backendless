aaa3e8b48932189c72f8f379a06a990d1c21cdfd
Olha Sheliakina <olya.shelyakina@gmail.com>
2023-10-26 21:05:17 +0300

    adding Projects Tasks component

--- src/tabs/ProjectsTasks.jsx
+++ src/tabs/ProjectsTasks.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import classNames from 'classnames';
 
 const ProjectsTasks = () => {
-  return (
-    <div>ProjectsTasks</div>
-  )
-}
+  const [projectsData, setProjectsData] = useState([]);
 
-export default ProjectsTasks
+  useEffect(() => {
+    fetch('http://localhost:3000/data/projectsData.json')
+      .then((response) => response.json())
+      .then((data) => {
+        setProjectsData(data.projectsTasks);
+      })
+      .catch((error) => {
+        console.error('Error has occured while fetching data:', error);
+      });
+  }, []);
+  return (
+    <section className='section is-medium'>
+      <ol>
+        {projectsData.map((project) => (
+          <div
+            key={project.id}
+            className={classNames({
+              'has-text-success': project.status === 'Completed',
+              'has-background-warning': project.status === 'In Progress',
+            })}
+          >
+            <li key={project.id}>
+              <span className='mx-6'>{project.name}</span>
+              <span>{project.status}</span>
+              <span className='mx-6'>{project.projectName}</span>
+            </li>
+          </div>
+        ))}
+      </ol>
+    </section>
+  );
+};
+
+export default ProjectsTasks;
